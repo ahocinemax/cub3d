a28f81425f33cf016b819bdff2fd4a2bce524812
Mariko TSUJI <tsujimarico@icloud.com>
2022-10-03 17:45:25 +0200

    place of file modified

--- srcs/game/init_game.c
+++ srcs/game/init_game.c
@@ -48,36 +48,3 @@ void	step_player(t_player *p1)
 		p1->side_dist_y = (p1->map_y + 1.0 - p1->pos_y) * p1->delta_dist_y;
 	}
 }
-
-void    dda_perform(g_game *game, t_player *p1)
-{
-
-      //perform DDA
-      while (hit == 0)
-      {
-        //jump to next map square, either in x-direction, or in y-direction
-        if (sideDistX < sideDistY)
-        {
-          sideDistX += deltaDistX;
-          mapX += stepX;
-          side = 0;
-        }
-        else
-        {
-          sideDistY += deltaDistY;
-          mapY += stepY;
-          side = 1;
-        }
-        //Check if ray has hit a wall
-        if (worldMap[mapX][mapY] > 0) hit = 1;
-      }
-}
-
-void    perpwalldist(t_player *p1)
-{
-    if (p1->side == 1)//sidedist:x
-        p1->perp_wall_dist = (p1->side_dist_x - p1->delta_dist_x);
-    else//sidedist:y
-        p1->perp_wall_dist = (p1->side_dist_y - p1->delta_dist_y);        
-    p1->line_height = (int)(WIN_H / p1->perp_wall_dist); 
-}
--- srcs/raycasting/raycasting.c
+++ srcs/raycasting/raycasting.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/cub3d.h"
 
+void    perpwall_dist(t_player *p1)
+{
+    if (p1->side == 1)//sidedist:x
+        p1->perp_wall_dist = (p1->side_dist_x - p1->delta_dist_x);
+    else//sidedist:y
+        p1->perp_wall_dist = (p1->side_dist_y - p1->delta_dist_y);        
+    p1->line_height = (int)(WIN_H / p1->perp_wall_dist); 
+}
+
+void    dda_perform(g_game *game, t_player *p1)
+{
+
+      //perform DDA
+      while (hit == 0)
+      {
+        //jump to next map square, either in x-direction, or in y-direction
+        if (sideDistX < sideDistY)
+        {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else
+        {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0) hit = 1;
+      }
+}
